13d5f7642ee5d426423c2c6557595862577a3f52
Kamilo Vasquez <killo.blanco@gmail.com>
2022-07-14 23:16:39 -0500

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/index.ts'),
+      name: 'Scaffold',
+      fileName: 'scaffold.js'
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', '@emotion/styled', '@emotion/react', '@mui/material'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          '@emotion/styled': 'styled',
+          '@emotion/react': 'emotionReact',
+          '@mui/material': 'material'
+        }
+      }
+    }
+  }
 })
