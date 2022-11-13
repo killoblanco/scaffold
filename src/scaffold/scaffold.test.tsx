e7232d52689ece4bf98507507341978d4d13e2b7
Kamilo Vasquez <killo.blanco@gmail.com>
2022-11-12 23:03:06 -0500

    ✨ Dialogs content connected

--- src/scaffold/scaffold.test.tsx
+++ src/scaffold/scaffold.test.tsx
@@ -1,16 +1,33 @@
-import { render, screen } from '@testing-library/react'
-import { describe, expect, it } from 'vitest'
-import { Scaffold } from './index'
+import { render, renderHook, screen } from '@testing-library/react'
+import { RecoilRoot } from 'recoil'
+import { afterAll, describe, expect, it } from 'vitest'
+import { Scaffold, useScaffold } from './index'
 
 describe('Scaffold Component', () => {
-
   it('Should render children', () => {
     render(
-      <Scaffold>
-        <div>Test</div>
-      </Scaffold>
+      <RecoilRoot>
+        <Scaffold>
+          <div>Test</div>
+        </Scaffold>
+      </RecoilRoot>
     )
 
     expect(screen.getByText('Test')).toBeTruthy()
+  })
+
+  describe('Hook', () => {
+    const { result, unmount } = renderHook(
+      () => useScaffold(),
+      { wrapper: RecoilRoot }
+    )
+
+    afterAll(unmount)
+
+    it('Should have dialogs with open and close fns', () => {
+      expect(result.current.dialogs).toBeDefined()
+      expect(result.current.dialogs.open).toBeDefined()
+      expect(result.current.dialogs.close).toBeDefined()
+    })
   })
 })
